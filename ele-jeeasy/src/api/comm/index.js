3efc9e866bcd72f236952361b214ba37fe037cbd
Alps <940086380@qq.com>
2021-07-15 14:30:13 +0800

    移除ele-jeeasy

--- ele-jeeasy/src/api/comm/index.js
+++
@@ -1,5 +0,0 @@
-import { get, post } from '_u/service'
-
-export const getToken = params => get('/getToken', params)
-
-export const navlist = data => post('/user/navlist', data)
--- ele-jeeasy/src/api/user/index.js
+++
@@ -1,5 +0,0 @@
-import { get, post } from '_u/service'
-
-export const getCurrLoginUser = () => get('/user/curr')
-
-export const loginByUsername = (data) => post('/login', data)
