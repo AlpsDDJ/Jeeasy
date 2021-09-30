b02a91f65821780d98457fca970c69e5eb279a4b
Alps <940086380@qq.com>
2021-09-30 17:52:29 +0800

    前端代码迁移到单独git仓库

--- jeeasy-antd/src/utils/commotSense.js
+++
@@ -1,7 +0,0 @@
-export const loginPath = '/user/login'
-
-export const authHeaderKey = 'JEEASY-ACCESS-TOKEN'
-
-export const localTokenKey = 'jeeasy-token'
-
-export const localRefreshTokenKey = 'jeeasy-refresh-token'
