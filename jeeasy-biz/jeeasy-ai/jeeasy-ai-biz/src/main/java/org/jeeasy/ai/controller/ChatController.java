e30d9cb8711e62daa564638539870041b2a70e62
DDJ <AlpsDDJ@163.com>
2024-08-04 22:36:43 +0800

    调整聊天控制器中的速率限制配置，将限制从每60秒5次调整为每24小时5次，并统一速率限制参数。

--- jeeasy-biz/jeeasy-ai/jeeasy-ai-biz/src/main/java/org/jeeasy/ai/controller/ChatController.java
+++ jeeasy-biz/jeeasy-ai/jeeasy-ai-biz/src/main/java/org/jeeasy/ai/controller/ChatController.java
@@ -16,6 +16,8 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.concurrent.TimeUnit;
+
 @Slf4j
 @RestController
 @RequestMapping("/ai/chat")
@@ -28,10 +30,12 @@ public class ChatController {
     //private final AiChatAppService chatAppService;
 
     private final static String RATE_LIMIT_KEY = "ai-chat";
+    private final static int RATE_LIMIT_MAX = 5;
+    private final static long RATE_LIMIT_TIME = 24L;
 
     @PostMapping
     @Operation(summary = "普通对话", description = "普通对话")
-    @RateLimit(max = 5, time = 60, key = RATE_LIMIT_KEY)
+    @RateLimit(max = RATE_LIMIT_MAX, time = RATE_LIMIT_TIME, key = RATE_LIMIT_KEY, unit = TimeUnit.HOURS)
     public Mono<R<ChatResponseVO>> chat(@RequestBody ChatMessageDTO messageDTO) {
         return Mono.justOrEmpty(aiChatService.chat(messageDTO));
     }
@@ -47,21 +51,21 @@ public class ChatController {
     @Deprecated
     @PostMapping(value = "/sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     @Operation(summary = "sse流式对话", description = "sse流式对话")
-    @RateLimit(max = 5, time = 60, key = RATE_LIMIT_KEY)
+    @RateLimit(max = RATE_LIMIT_MAX, time = RATE_LIMIT_TIME, key = RATE_LIMIT_KEY, unit = TimeUnit.HOURS)
     public SseEmitter chatSse(@RequestBody ChatMessageDTO messageDTO) {
         return aiChatService.streamChat(messageDTO);
     }
 
     @GetMapping(value = "/test", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     @Operation(summary = "流式对话", description = "流式对话")
-    @RateLimit(max = 5, time = 60, key = RATE_LIMIT_KEY)
+    @RateLimit(max = RATE_LIMIT_MAX, time = RATE_LIMIT_TIME, key = RATE_LIMIT_KEY, unit = TimeUnit.HOURS)
     public SseEmitter test() {
         return aiChatService.streamChat(ChatMessageDTO.withMessage("你好！"));
     }
 
     @PostMapping(value = "/stream")
     @Operation(summary = "流式对话", description = "流式对话")
-    @RateLimit(max = 5, time = 60, key = RATE_LIMIT_KEY)
+    @RateLimit(max = RATE_LIMIT_MAX, time = RATE_LIMIT_TIME, key = RATE_LIMIT_KEY, unit = TimeUnit.HOURS)
     public SseEmitter streamChat(@RequestBody ChatMessageDTO messageDTO) {
         SseEmitter sseEmitter = aiChatService.streamChat(messageDTO);
         //return Mono.justOrEmpty(sseEmitter);
